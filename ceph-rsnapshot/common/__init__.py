5fd0422f2f994ceb06aa3e8b1c10c4c955e0b3a2
John Noss <noss@harvard.edu>
2016-06-06 15:06:59 -0400

    wip move setup_logging

---
+++ ceph-rsnapshot/common/__init__.py
@@ -0,0 +1,4 @@
+
+__all__ = [
+  "setup_logging"
+]
---
+++ ceph-rsnapshot/common/setup_logging.py
@@ -0,0 +1,42 @@
+# logging setup
+
+def setup_logging(log_filename, log_location='/var/log/ceph-rsnapshot/', verbose=False):
+  logger = logging.getLogger(__name__)
+  # get logger for sh module so we can configure it as well
+  sh_logger = logging.getLogger('sh.command')
+
+  log_level = logging.INFO
+  if verbose == True:
+    log_level = logging.DEBUG
+  if not os.path.isdir(log_location):
+    os.makedirs(log_location)
+  log_file = "%s/%s.log" % (log_location, log_filename)
+  # logging.basicConfig(filename=log_file, level=log_level)
+
+  logger.setLevel(log_level)
+  if sh_logging:
+    sh_logger.setLevel(log_level)
+
+  # setup log format
+  pid = os.getpid()
+  hostname = socket.gethostname()
+  LOG_FORMAT = ("%(asctime)s [" + hostname + " PID:" + str(pid) +
+                "] [%(levelname)-5.5s] [%(name)s] %(message)s")
+  # LOG_FORMAT = ("%(asctime)s [" + str(pid) +
+  #               "] [%(levelname)-5.5s] [%(name)s] %(message)s")
+  logFormatter = logging.Formatter(LOG_FORMAT)
+
+  # set format on file loggers
+  fileHandler = logging.FileHandler(log_file)
+  fileHandler.setFormatter(logFormatter)
+  logger.addHandler(fileHandler)
+  if sh_logging:
+    sh_logger.addHandler(fileHandler)
+
+  # setup console loggers
+  consoleHandler = logging.StreamHandler(stream=sys.stderr)
+  consoleHandler.setFormatter(logFormatter)
+  logger.addHandler(consoleHandler)
+  if sh_logging:
+    sh_logger.addHandler(consoleHandler)
+  return(logger)
