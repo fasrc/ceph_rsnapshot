62567abe2d11a91b4d96c37abfa50dc421d9c5ea
John Noss <noss@harvard.edu>
2016-06-10 13:58:43 -0400

    logging and variable naming in remove qcow

--- ceph_rsnapshot/utils/remove_qcow.py
+++ ceph_rsnapshot/utils/remove_qcow.py
@@ -9,25 +9,27 @@ from ceph_rsnapshot import logs
 from ceph_rsnapshot import settings
 
 def remove_qcow():
-  parser = argparse.ArgumentParser(description='deletes a temporary qcow')
+  parser = argparse.ArgumentParser(description='deletes a temporary qcow',
+                                   argument_default=argparse.SUPPRESS)
   parser.add_argument('image')
-  parser.add_argument('--pool', default='rbd')
+  parser.add_argument('--pool', required=False)
   # parser.add_argument('--sum', dest='accumulate', action='store_const',
   #                     const=sum, default=max,
   #                     help='sum the integers (default: find the max)')
   args = parser.parse_args()
   image = args.image
-  pool = args.pool
+
+  if args.__contains__('pool'):
+    settings.POOL = args.pool
 
   settings.load_settings()
-  settings.POOL = pool
 
   logger = logs.setup_logging()
 
-  logger.info("deleting temp qcow for image %s from pool %s " % (image, pool))
+  logger.info("deleting temp qcow for image %s from pool %s " % (image, settings.POOL))
 
   try:
-    os.remove("%s/%s/%s.qcow2" % (settings.QCOW_TEMP_PATH, pool, image))
+    os.remove("%s/%s/%s.qcow2" % (settings.QCOW_TEMP_PATH, settings.POOL, image))
   except Exception as e:
     logger.error(e.stdout)
     logger.error(e.stderr)
